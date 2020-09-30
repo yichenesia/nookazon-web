ea5c74a21b10fe76393340a41bf9b74a2d18f5d5
Chloe Zhao <yichenesia@gmail.com>
2020-09-29 22:21:09 -0400

    Finished implementing checkout page

--- mybackend/index.js
+++ mybackend/index.js
@@ -6,6 +6,18 @@ app.use(cors())
 const port = 5000
 const fs = require('fs');
 
+app.post('/saveuserorder', (req, res) => {
+  console.log(req);
+  console.log(req.body);
+  const content = req.body;
+  fs.writeFile('userorder.json', content, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    //file written successfully
+  })
+})
 
 app.get('/productlist', (req, res) => {
     let rawdata = fs.readFileSync('productlist.json');
--- myfrontend/src/App.js
+++ myfrontend/src/App.js
@@ -1,17 +1,34 @@
 import React, {Component} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import './App.css';
 import {Services} from './Services';
 import CartItem from './CartItem';
 
+
 var services = new Services();
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
         productlist: [],
+        shoppingCartList: [],
         showProductListPage: true,
-        showCheckoutPage: false
+        showCheckoutPage: false,
+        total: {"subtotal": 0, "tax": 0, "linetotal": 0, "yousave": 0}
     }
   }
 
@@ -30,11 +47,46 @@ class App extends Component {
   };
 
   togglePage = () => {
+    
     this.setState({
       showProductListPage: !this.state.showProductListPage,
       showCheckoutPage: !this.state.showCheckoutPage,
+      
     })
-    var shoppingCartList = this.state.productlist.filter(item => item.count > 0);
+
+    var result = this.calculate(0.0);
+    this.setState({
+      shoppingCartList: result.shoppingList,
+      total: result.totalList
+    })
+  }
+
+  calculate = (discount) => {
+    var totalList = {"subtotal": 0, "tax": 0, "linetotal": 0, "yousave": 0}
+    var shoppingList = this.state.productlist.filter(item => item.count > 0)
+
+    shoppingList.map((item) => {
+      item.yousave = item.price * item.count * discount;
+      item.subtotal = item.price * item.count * (1 - discount);
+      item.tax = item.subtotal * 0.13;
+      item.linetotal = item.subtotal + item.tax;
+      
+      totalList.yousave += item.yousave;
+      totalList.subtotal += item.subtotal;
+      totalList.tax += item.tax;
+      totalList.linetotal += item.linetotal;
+    })
+
+    return {totalList, shoppingList}
+    
+  }
+  
+  handleDiscountAll = () => {
+    var result = this.calculate(0.5);
+    this.setState({
+      shoppingCartList: result.shoppingList,
+      total: result.totalList
+    })
   }
 
   render(){
@@ -46,14 +98,52 @@ class App extends Component {
           {this.state.productlist.map((value, index) => (<CartItem data={value}/>))}
           <button onClick={this.togglePage}>Checkout</button>
         </div>}
-        {this.state.showCheckoutPage &&
-        <div>
+        {this.state.showCheckoutPage && <div>
+        <button onClick={this.handleDiscountAll}>Add 50% Discount to all items</button>
+        {/* <button>Add 70% Discount to Nintendo Switch</button> */}
+        <TableContainer component={Paper}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Product Name</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Amount</TableCell>
+              <TableCell align="right">Subtotal</TableCell>
+              <TableCell align="right">Tax</TableCell>
+              <TableCell align="right">Line Total</TableCell>
+              <TableCell align="right">You Save</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.shoppingCartList.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">${row.price}</TableCell>
+                <TableCell align="right">{row.count}</TableCell>
+                <TableCell align="right">${row.subtotal}</TableCell>
+                <TableCell align="right">${row.tax}</TableCell>
+                <TableCell align="right">${row.linetotal}</TableCell>
+                <TableCell align="right">${row.yousave}</TableCell>
+              </TableRow>
+            ))}
+            <TableRow>
+                <TableCell component="th" scope="row">
+                 Total
+                </TableCell>
+                <TableCell align="right"></TableCell>
+                <TableCell align="right"></TableCell>
+                <TableCell align="right">${this.state.total.subtotal}</TableCell>
+                <TableCell align="right">${this.state.total.tax}</TableCell>
+                <TableCell align="right">${this.state.total.linetotal}</TableCell>
+                <TableCell align="right">${this.state.total.yousave}</TableCell>
+              </TableRow>
+          </TableBody>
           
+        </TableContainer>
           <button onClick={this.togglePage}>Back to Products</button>
         </div>}
         
-
-
       </div>)
   }
 }
