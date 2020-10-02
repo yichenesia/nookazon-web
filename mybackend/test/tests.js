7c38f5495c272913eee6b0aac60ae944a167c1a9
Chloe Zhao <yichenesia@gmail.com>
2020-10-02 16:08:23 -0400

    Update tests.js

--- mybackend/test/tests.js
+++ mybackend/test/tests.js
@@ -6,7 +6,7 @@ const { expect } = chai
 chai.use(chaiHttp)
 
 it('Homepage access success', async () => {
-    chai.request('http://localhost:5000')
+    chai.request('https://a1-nookazon-backend.herokuapp.com/')
             .get('/')
             .end(function (err, res) {
                 const result = res.statusCode
@@ -17,7 +17,7 @@ it('Homepage access success', async () => {
   }).timeout(15000)
 
 it('Product List connected', async () => {
-  chai.request('http://localhost:5000/productlist')
+  chai.request('https://a1-nookazon-backend.herokuapp.com/productlist')
           .get('/')
           .end(function (err, res) {
               const result = res.statusCode
